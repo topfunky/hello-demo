a5305ecdce4e4eca1813d7a8bfef4dd35fce9100
Geoffrey Grosenbach <boss@topfunky.com>
2013-12-12 21:58:41 -0800

    First commit.

---
+++ test/hello-test.js
@@ -0,0 +1,11 @@
+var hello = require('../hello');
+var assert = require('assert');
+
+describe('hello', function() {
+
+  it("returns the string 'hello'", function() {
+    var result = hello();
+    assert.equal(result, 'hello');
+  });
+
+});
