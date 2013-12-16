4b60210351689d5ac4527e7f5a1ada1b7baa9fb1
Geoffrey Grosenbach <boss@topfunky.com>
2013-12-15 22:57:24 -0800

    Fix build.

--- test/hello-test.js
+++ test/hello-test.js
@@ -5,7 +5,7 @@ describe('hello', function() {
 
   it("returns the string 'hello'", function() {
     var result = hello();
-    assert.equal(result, 'hellooo');
+    assert.equal(result, 'hello');
   });
 
 });
